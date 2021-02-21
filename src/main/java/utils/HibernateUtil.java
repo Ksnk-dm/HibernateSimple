139d5ea33fa2c3fd71b26d22536af9ee0196e38b
Dmitry <71468576+Ksnk-dm@users.noreply.github.com>
2021-02-21 15:07:00 +0200

    Update HibernateUtil.java

--- src/main/java/utils/HibernateUtil.java
+++ src/main/java/utils/HibernateUtil.java
@@ -9,7 +9,7 @@ public class HibernateUtil {
     private static SessionFactory buildSessionFactory() {
         try{
             return new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
-        }catch (Exception e){
+        }catch (ConfigurationException e){
             throw new ExceptionInInitializerError(e);
         }
     }
